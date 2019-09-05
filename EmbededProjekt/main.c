655b8e5c36e386e670c9a19116c6ac1dfcb22b0d
Kevin Pike Darmer <amopeika@gmail.com>
2019-09-05 14:00:31 +0200

    Init Commit

--- EmbededProjekt/main.c
+++ EmbededProjekt/main.c
@@ -1,3 +1,16 @@
+/**
+*	Titel: main.c
+*
+*	Author: Kevin Pike Darmer
+*
+*	main method
+*
+*	ColumnScan() sets the collumns to low one by one
+*	ReadRows() reads if a button is pressed for each row one by one
+*	DecodeKeyboard() decodes what char was pressed by looking up collumn and row number in multidimensional array
+*	menu() shows the menu and send the parameters to change in case of button press
+*
+*/
 #include <stdio.h>
 #include "stdio_setup.h"
 #include "metrixKeypadLib.h"
